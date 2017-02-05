03b0197582f3c7266804ea4e55f99b2af64bd78b
NickolasBoyer <rut.willems@gmail.com>
2017-02-05 10:38:33 +0100

    Moved the login input to an external component

--- app/src/components/loginView.vue
+++ app/src/components/loginView.vue
@@ -24,74 +24,6 @@
   transform: translate(-50%, -50%);
 }
 
-// Shamelesly copied from: http://codepen.io/anon/pen/egKded
-.material {
-	position: relative;
-	padding: 0;
-	margin: 5px;
-	border: none;
-	overflow: visible;
-
-	input {
-		box-sizing: border-box;
-		width: 100%;
-		padding: 12px 10px 8px;
-		border: none;
-		border-radius: 0;
-		box-shadow: none;
-		border-bottom: 1px solid #DDD;
-		font-size: 120%;
-		outline: none;
-		cursor: text;
-
-		&::-webkit-input-placeholder {
-			transition: color 300ms ease;
-		}
-		&:not(:focus)::-webkit-input-placeholder {
-			color: transparent;
-		}
-	}
-
-	hr {
-		content: '';
-		display: block;
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		margin: 0;
-		padding: 0;
-		width: 100%;
-		height: 2px;
-		border: none;
-		background: $prim-red;
-		font-size: 1px;
-		will-change: transform, visibility;
-		transition: all 200ms ease-out;
-		transform: scaleX(0);
-		visibility: hidden;
-		z-index: 10;
-	}
-	input:focus ~ hr {
-		transform: scaleX(1);
-		visibility: visible;
-	}
-
-	label {
-		position: absolute;
-		top: 10px;
-		left: 10px;
-		font-size: 120%;
-		color: $prim-red;
-		transform-origin: 0 -150%;
-		transition: transform 300ms ease;
-		pointer-events: none;
-	}
-	input:focus ~ label,
-	input:valid ~ label {
-		transform: scale(0.6);
-	}
-}
-
 .settings-title {
   font-size: 15px;
   text-align: center;
@@ -116,28 +48,24 @@
     .form-wrapper
       div(class="settings-title")
         h1 Settings
-      fieldset(class="material")
-        input(v-model="APIKey", type="text", placeholder="e.g. VYFIKA2D1FMHMOP7", required)
-        hr
-        label APIkey
-      fieldset(class="material")
-        input(v-model="username", type="text", placeholder="e.g. stormagddn123", required)
-        hr
-        label Username
-      fieldset(class="material")
-        input(v-model="userkey", type="text", placeholder="e.g. 841212", required)
-        hr
-        label Userkey
+      material-input(v-model="APIKey", :label="'APIkey'", :placeholder="'e.g. VYFIKA2D1FMHMOP7'")
+      material-input(v-model="username", :label="'Username'", :placeholder="'e.g. stormagddn123'")
+      material-input(v-model="userkey", :label="'Userkey'", :placeholder="'e.g. 841212'")
     router-link(class="back-button", to="/")
       i(class="material-icons") arrow_back
 </template>
 --------------------------------------------------------------------------------
 <script>
+import MaterialInput from './LoginView/MaterialInput';
+
 export default {
   name: 'login',
   data () {
     return {
     };
+  },
+  components: {
+    MaterialInput
   },
   computed: {
     APIKey: {
---
+++ app/src/components/loginView/MaterialInput.vue
@@ -0,0 +1,99 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../../assets/sass/colors";
+
+// Shamelesly copied from: http://codepen.io/anon/pen/egKded
+.material {
+	position: relative;
+	padding: 0;
+	margin: 5px;
+	border: none;
+	overflow: visible;
+
+	input {
+		box-sizing: border-box;
+		width: 100%;
+		padding: 12px 10px 8px;
+		border: none;
+		border-radius: 0;
+		box-shadow: none;
+		border-bottom: 1px solid #DDD;
+		font-size: 120%;
+		outline: none;
+		cursor: text;
+
+		&::-webkit-input-placeholder {
+			transition: color 300ms ease;
+		}
+		&:not(:focus)::-webkit-input-placeholder {
+			color: transparent;
+		}
+	}
+
+	hr {
+		content: '';
+		display: block;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 2px;
+		border: none;
+		background: $prim-red;
+		font-size: 1px;
+		will-change: transform, visibility;
+		transition: all 200ms ease-out;
+		transform: scaleX(0);
+		visibility: hidden;
+		z-index: 10;
+	}
+	input:focus ~ hr {
+		transform: scaleX(1);
+		visibility: visible;
+	}
+
+	label {
+		position: absolute;
+		top: 10px;
+		left: 10px;
+		font-size: 120%;
+		color: $prim-red;
+		transform-origin: 0 -150%;
+		transition: transform 300ms ease;
+		pointer-events: none;
+	}
+	input:focus ~ label,
+	input:valid ~ label {
+		transform: scale(0.6);
+	}
+}
+</style>
+--------------------------------------------------------------------------------
+<template lang="pug">
+  fieldset(class="material")
+    input(v-bind:value="value",
+          v-on:input="updateValue($event.target.value)",
+          type="text",
+          placeholder="e.g. VYFIKA2D1FMHMOP7",
+          required)
+    hr
+    label {{label}}
+</template>
+--------------------------------------------------------------------------------
+<script>
+export default {
+  name: 'seasonSelector',
+  props: [
+    'value',
+    'label',
+    'placeholder'
+  ],
+  methods: {
+    updateValue: function (value) {
+      this.$emit('input', value);
+    }
+  }
+};
+</script>
